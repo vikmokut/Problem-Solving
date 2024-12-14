cbb7487e540e07075e285e7be97f6c7293d8dd88
Victor Mokut <vikmokut@gmail.com>
2024-12-14 22:25:40 +0100

    new

---
+++ c++/w3s/banking_app/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/* 
+ * ***in development***
+class: banking_service
+attributes:
+    account_name
+    account_number
+    account_balance
+    deposit_amount
+    withdrawal_amount
+methods:
+    withdrawal
+    deposit
+    check_balance
+*/
